2b59181db77ed3d1188c469effb6cd9b9ea083a2
ivngitlab <ivn@li.ru>
2018-02-10 01:12:00 +0300

    env module

---
+++ portfolio/portfolio/settings/env.py
@@ -0,0 +1,24 @@
+import os
+from configparser import ConfigParser
+
+
+# instantiate
+config = ConfigParser()
+
+# parse existing file
+config.read('config.ini')
+
+
+def set_env():
+    try:
+        # [DB]
+        os.environ.setdefault('DB_HOST', config.get('DB', 'DB_HOST'))
+        os.environ.setdefault('DB_PORT', config.get('DB', 'DB_PORT'))
+        os.environ.setdefault('DB_NAME', config.get('DB', 'DB_NAME'))
+        os.environ.setdefault('DB_USER', config.get('DB', 'DB_USER'))
+        os.environ.setdefault('DB_PASS', config.get('DB', 'DB_PASS'))
+
+        # [SECRET]
+        os.environ.setdefault('SECRET_KEY', config.get('SECRET', 'SECRET_KEY'))
+    except Exception as e:
+        return e
--- portfolio/portfolio/settings/production.py
+++ portfolio/portfolio/settings/production.py
@@ -1,8 +1,11 @@
 from __future__ import absolute_import, unicode_literals
+from .env import set_env
 
 from .base import *
 
 DEBUG = False
+
+set_env()
 
 try:
     from .local import *

2300b6331fb0b5d98b2d56289147060bcb95e9e6
ivngitlab <ivn@li.ru>
2018-02-10 01:49:05 +0300

    env module

--- portfolio/portfolio/settings/env.py
+++ portfolio/portfolio/settings/env.py
@@ -1,4 +1,4 @@
-import os
+import subprocess
 from configparser import ConfigParser
 
 
@@ -12,14 +12,15 @@ config.read('config.ini')
 def set_env():
     try:
         # [DB]
-        os.environ.setdefault('DB_HOST', config.get('DB', 'DB_HOST'))
-        os.environ.setdefault('DB_PORT', config.get('DB', 'DB_PORT'))
-        os.environ.setdefault('DB_NAME', config.get('DB', 'DB_NAME'))
-        os.environ.setdefault('DB_USER', config.get('DB', 'DB_USER'))
-        os.environ.setdefault('DB_PASS', config.get('DB', 'DB_PASS'))
+        subprocess.call(["export {}={}".format('DB_HOST', config.get('DB', 'DB_HOST'))])
+        subprocess.call(["export {}={}".format('DB_PORT', config.get('DB', 'DB_PORT'))])
+        subprocess.call(["export {}={}".format('DB_NAME', config.get('DB', 'DB_NAME'))])
+        subprocess.call(["export {}={}".format('DB_USER', config.get('DB', 'DB_USER'))])
+        subprocess.call(["export {}={}".format('DB_PASS', config.get('DB', 'DB_PASS'))])
 
         # [SECRET]
-        os.environ.setdefault('SECRET_KEY', config.get('SECRET', 'SECRET_KEY'))
+        subprocess.call(["export {}={}".format('SECRET_KEY', config.get('SECRET', 'SECRET_KEY'))])
+
         return False
     except Exception as e:
         return e

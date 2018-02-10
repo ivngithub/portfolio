848a9be43da88ffc96ca9f61f7ac01fcb30c3572
ivngitlab <ivn@li.ru>
2018-02-10 20:33:46 +0300

    added env const for hosts

--- portfolio/portfolio/settings/production.py
+++ portfolio/portfolio/settings/production.py
@@ -4,6 +4,11 @@ from .base import *
 
 DEBUG = False
 
+
+PUBLIC_HOST = os.environ.get('PUBLIC_HOST')
+PRIVATE_HOST = os.environ.get('PRIVATE_HOST')
+ALLOWED_HOSTS = [PUBLIC_HOST, PRIVATE_HOST]
+
 SECRET_KEY = os.environ.get('SECRET_KEY', 'asdko_UYIUnm_1231_ADSd=-8!sss'),
 
 try:

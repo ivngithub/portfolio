240953349190e72d36bc5e2b37c444374094f8c1
ivngitlab <ivn@li.ru>
2018-05-02 21:57:46 +0300

    checkout -b microprojects

--- microproject/identicons/app/identidock.py
+++ microproject/identicons/app/identidock.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Docker! And thanks Compose!\n'
+    return 'Hello Docker!\nAnd thanks Compose!\n'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')

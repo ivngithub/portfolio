2dd69d796141c39aaf6da3d30b9be0d3d55f60d0
ivngitlab <ivn@li.ru>
2018-05-03 21:39:30 +0300

    edited docker-compose file and added key --link

--- microproject/identicons/app/identidock.py
+++ microproject/identicons/app/identidock.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, Response
+import requests
+
 app = Flask(__name__)
+default_name = 'ivn'
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello Docker!\nAnd thanks Compose!\n'
+def mainpage():
+    name = default_name
+
+    header = '<html><head><title></title></head><body>'
+    body = '''<form method="POST">
+            Hello <input type="text" name="name" value="{}">
+            <input type="submit" value="submit">
+            </form>
+            <p>You look like a:
+            <img src="/monster/monster.png"/>               
+    '''.format(name)
+    footer ='</body></html>'
+
+    return header + body + footer
+
+
+@app.route('/monster/<name>')
+def get_identicon(name):
+    r = requests.get('http://dnmonster:8080/monster/' + name + '?size=160')
+    image = r.content
+
+    return Response(image, mimetype='image/png')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')

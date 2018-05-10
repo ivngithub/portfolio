d84220934d08159292ca17cb70823b17319110e8
ivngitlab <ivn@li.ru>
2018-05-10 22:02:27 +0300

    deploy

--- microproject/identicons/app/identidock.py
+++ microproject/identicons/app/identidock.py
@@ -1,22 +1,32 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 import requests
+import hashlib
+import redis
+
 
 app = Flask(__name__)
+cache = redis.StrictRedis(host='redis', port=6379, db=0)
+salt = 'Unique_salt'
 default_name = 'ivn'
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def mainpage():
+
     name = default_name
+    if request.method == 'POST':
+        name = request.form['name']
+    salted_name = salt + name
+    name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
 
     header = '<html><head><title></title></head><body>'
     body = '''<form method="POST">
-            Hello <input type="text" name="name" value="{}">
-            <input type="submit" value="submit">
-            </form>
-            <p>You look like a:
-            <img src="/monster/monster.png"/>               
-    '''.format(name)
+        Hello <input type="text" name="name" value="{}">
+        <input type="submit" value="submit">
+        </form>
+        <p>You look like a:
+        <img src="/monster/{}"/>               
+    '''.format(name, name_hash)
     footer ='</body></html>'
 
     return header + body + footer
@@ -24,8 +34,13 @@ def mainpage():
 
 @app.route('/monster/<name>')
 def get_identicon(name):
-    r = requests.get('http://dnmonster:8080/monster/' + name + '?size=160')
-    image = r.content
+
+    image = cache.get(name)
+    if image is None:
+        print('Cache is miss')
+        r = requests.get('http://dnmonster:8080/monster/' + name + '?size=160')
+        image = r.content
+        cache.set(name, image)
 
     return Response(image, mimetype='image/png')
 
--- microproject/identicons/cmd.sh
+++ microproject/identicons/cmd.sh
@@ -6,6 +6,6 @@ if [ "$ENV" = 'DEV' ]; then
   exec python "identidock.py"
 else
   echo "Running Production Server"
-  exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py \
+  exec uwsgi --http 127.0.0.1:5000 --wsgi-file /app/identidock.py \
              --callable app --stats 0.0.0.0:9191
 fi

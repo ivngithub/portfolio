4108327bff9f61dbc13496f678f07340cc1c1814
ivngitlab <ivn@li.ru>
2018-05-11 23:33:25 +0300

    added micropojects on Flask

--- front/src/Nav.vue
+++ front/src/Nav.vue
@@ -11,7 +11,7 @@
       <li><div class="divider"></div></li>
       <li><h4 class="h4-p">projects:</h4></li>
       <li><a class="waves-effect" href="/slider" target="_parent">Slider</a></li>
-      <li><a class="waves-effect" href="http://microprojects.ivn.website" target="_parent"><h2 class="h2-p">microprojects</h2></a></li>
+      <li><a href="http://microprojects.ivn.website" target="_parent"><h2 class="h2-p">microprojects</h2></a></li>
     </ul>
     <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
   </div>

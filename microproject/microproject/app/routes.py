a42aa60b6db09e6b33b3226c9520d9b54664b2a5
ivngitlab <ivn@li.ru>
2018-06-30 23:39:30 +0300

    added login and registration function

--- microproject/microproject/app/routes.py
+++ microproject/microproject/app/routes.py
@@ -1,23 +1,18 @@
-from flask import Response, render_template, request, flash, redirect, url_for
 import requests
 import hashlib
 import redis
 import psycopg2
-from app import app
-from app.forms import LoginForm
+
+from flask import Response, render_template, request, flash, redirect, url_for
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
+
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
 
 
 cache = redis.StrictRedis(host='redis', port=6379, db=0)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
 
 
 @app.route('/')
@@ -30,7 +25,6 @@ def index():
 
 @app.route('/you-are')
 def you_are():
-
     ip_address = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
     user_agent = request.user_agent.string
     user_identification = ip_address + user_agent
@@ -40,14 +34,13 @@ def you_are():
 
 
 @app.route('/microproject-settings')
+@login_required
 def microproject_settings():
-
     return render_template('microproject_settings.html', context=locals())
 
 
 @app.route('/monster/<name>')
 def get_identicon(name):
-
     image = cache.get(name)
     if image is None:
         print('Cache is miss')
@@ -56,3 +49,42 @@ def get_identicon(name):
         cache.set(name, image)
 
     return Response(image, mimetype='image/png')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)

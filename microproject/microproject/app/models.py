9dd13ddcaf5e6ac76a67840ef3719ab76ff78025
ivngitlab <ivn@li.ru>
2018-07-12 22:05:11 +0300

    added follower functional

--- microproject/microproject/app/models.py
+++ microproject/microproject/app/models.py
@@ -7,6 +7,12 @@ from flask_login import UserMixin
 from app import db, login
 
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -15,6 +21,12 @@ class User(UserMixin, db.Model):
     few_feedback = db.relationship('Feedback', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -25,6 +37,25 @@ class User(UserMixin, db.Model):
     @property
     def user_hash_for_avatar(self):
         return hashlib.sha256(self.email.encode()).hexdigest()
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def followed_few_feedback(self):
+        followed = Feedback.query.join(
+            followers, (followers.c.followed_id == Feedback.user_id)).filter(
+            followers.c.follower_id == self.id)
+        own = Feedback.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Feedback.timestamp.desc())
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
